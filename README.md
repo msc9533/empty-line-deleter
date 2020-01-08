68880aa148bbdad4c88841a93c5bd2a59d333c04
msc9533 <msc9533@gmail.com>
2020-01-08 23:24:33 +0900

    initialized source

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # empty-line-deleter
+
+## Download .exe file
+<a id="raw-url" href="emptylinedeleter.exe">Download for Windows</a>
---
+++ source.py
@@ -0,0 +1,26 @@
+import pyperclip
+from tkinter import *
+
+root = Tk()
+root.attributes('-topmost', 'true')
+
+def click():
+    result_str = ""
+    in_str = pyperclip.paste()
+    print(in_str)
+    temp_str = ""
+    for ch in in_str:
+        if ch != '\n' and ord(ch) != 13:
+            temp_str = temp_str + ch
+        elif ch == '\n':
+            temp_str = temp_str + ' '
+
+    result_str = temp_str
+    pyperclip.copy(result_str)
+ 
+frame = Frame(root, width=100, height=30)
+btn = Button(text = "Press", command = click)
+btn.pack()
+root.lift()
+frame.pack()
+mainloop()
